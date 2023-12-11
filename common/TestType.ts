51c2ae4877aac0288f289c353583adb040312236
Jamie Chong <jamie@sendage.com>
2023-12-10 16:41:33 -0800

    use shared common folder and use test type and enum

---
+++ common/TestType.ts
@@ -0,0 +1,4 @@
+export type TestType = {
+  id: number
+  email: string,
+};
--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,6 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Types work
+import { TestType } from "../common/TestType";
+const me: TestType = {
+  id: 1,
+  email: "asdf",
+}
+
+// Enums don't - comment these lines out and watch the app build fine
+import { TestEnum } from "../common/TestEnum";
+const thing:TestEnum = TestEnum.One;
+
+
 export default function App() {
   return (
     <View style={styles.container}>
